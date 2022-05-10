a4c2c51465f320bcfc4812425ecd66b14409e711
mustapha jay <mustaphajay2@gmail.com>
2022-05-10 23:31:56 +0100

    add headings and links

--- stripe/src/Sidebar.jsx
+++ stripe/src/Sidebar.jsx
@@ -1,5 +1,7 @@
 import React from "react";
+
 import { useGlobal } from "./Content";
+import sublinks from "./data";
 
 const Sidebar = () => {
   const { isSidebarOpen, closeSidebar } = useGlobal();
@@ -14,6 +16,27 @@ const Sidebar = () => {
           <button className="close-btn" onClick={closeSidebar}>
             X
           </button>
+          <div className="sidebar-links">
+            {sublinks.map((item, index) => {
+              const { links, page } = item;
+              return (
+                <article key={index}>
+                  <h4>{page}</h4>
+                  <div className="sidebar-sublinks">
+                    {links.map((link, index) => {
+                      const { url, icon, label } = link;
+                      return (
+                        <a key={index} href={url}>
+                          {icon}
+                          {label}
+                        </a>
+                      );
+                    })}
+                  </div>
+                </article>
+              );
+            })}
+          </div>
         </div>
       </aside>
     </div>
