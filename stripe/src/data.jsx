c8ba8061eef6e6af47e62dcf8ee91def014eb7b2
mustapha jay <mustaphajay2@gmail.com>
2022-05-10 15:06:39 +0100

    add input data

---
+++ stripe/src/data.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+const sublinks = [
+  {
+    page: "products",
+    links: [
+      {
+        label: "payment",
+        icon: <i className="fa-solid fa-bars text-2xl"></i>,
+        url: "/products",
+      },
+      {
+        label: "terminal",
+        icon: <i className="fa-solid fa-bars text-2xl"></i>,
+        url: "/products",
+      },
+      {
+        label: "connect",
+        icon: <i className="fa-solid fa-bars text-2xl"></i>,
+        url: "/products",
+      },
+    ],
+  },
+  {
+    page: "developers",
+    links: [
+      {
+        label: "plugins",
+        icon: <i className="fa-solid fa-bars text-2xl"></i>,
+        url: "/products",
+      },
+      {
+        label: "libraries",
+        icon: <i className="fa-solid fa-bars text-2xl"></i>,
+        url: "/products",
+      },
+      {
+        label: "help",
+        icon: <i className="fa-solid fa-bars text-2xl"></i>,
+        url: "/products",
+      },
+      {
+        label: "billing",
+        icon: <i className="fa-solid fa-bars text-2xl"></i>,
+        url: "/products",
+      },
+    ],
+  },
+  {
+    page: "company",
+    links: [
+      {
+        label: "about",
+        icon: <i className="fa-solid fa-bars text-2xl"></i>,
+        url: "/products",
+      },
+      {
+        label: "customers",
+        icon: <i className="fa-solid fa-bars text-2xl"></i>,
+        url: "/products",
+      },
+    ],
+  },
+];
+
+export default sublinks;
