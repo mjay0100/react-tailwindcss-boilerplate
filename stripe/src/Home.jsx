f677d9b2151f1941b21af2aa4e59593d75e73937
mustapha jay <mustaphajay2@gmail.com>
2022-05-11 13:44:48 +0100

    add submenu state and passed function

--- stripe/src/Home.jsx
+++ stripe/src/Home.jsx
@@ -8,10 +8,14 @@ const Home = () => {
     <section className="hero">
       <div className="hero-center">
         <article className="hero-info">
-          <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h1>
+          <h1>
+            Payments infrastructure <br />
+            for the internet
+          </h1>
           <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Pariatur,
-            dolore.
+            Millions of companies of all sizes—from startups to Fortune 500s—use
+            Stripe’s software and APIs to accept payments, send payouts, and
+            manage their businesses online.
           </p>
           <button className="btn">Start now</button>
         </article>
