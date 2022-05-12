e3262e205d23161d8403de1e1cf1d9a91eb6702c
mustapha jay <mustaphajay2@gmail.com>
2022-05-12 05:06:24 +0100

    handle dynamic submenu by changing location onMouseOver

--- stripe/src/Submenu.jsx
+++ stripe/src/Submenu.jsx
@@ -1,7 +1,48 @@
-import React from "react";
+import React, { useState, useRef, useEffect } from "react";
+import { useGlobal } from "./Content";
 
 const Submenu = () => {
-  return <div>Submenu</div>;
-};
+  const {
+    isSubmenuOpen,
+    page: { page, links },
+    location,
+  } = useGlobal();
+  const container = useRef(null);
+  const [columns, setColumns] = useState("col-2");
+  useEffect(() => {
+    setColumns("col-2");
+    const submenu = container.current;
+    const { center, bottom } = location;
+    submenu.style.left = `${center}px`;
+    submenu.style.top = `${bottom}px`;
 
+    if (links.length === 3) {
+      setColumns("col-3");
+    }
+    if (links.length > 3) {
+      setColumns("col-4");
+    }
+  }, [page, location, links]);
+  return (
+    <aside
+      className={`${isSubmenuOpen ? "submenu show" : "submenu"}`}
+      ref={container}
+    >
+      <section>
+        <h4>{page}</h4>
+        <div className={`submenu-center ${columns}`}>
+          {links.map((link, index) => {
+            const { url, icon, label } = link;
+            return (
+              <a key={index} href={url}>
+                {icon}
+                {label}
+              </a>
+            );
+          })}
+        </div>
+      </section>
+    </aside>
+  );
+};
 export default Submenu;
